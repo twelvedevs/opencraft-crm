28eb0dba7cdc3c6d406b00aae62cb3fae08b4a56
Andrey Pankov <a.pankov@gmail.com>
2026-04-03 15:37:09 +0300

    chore: fix knexfiles to use DATABASE_URL and flat config for docker-compose

--- apps/platform/messaging/knexfile.ts
+++ apps/platform/messaging/knexfile.ts
@@ -1,39 +1,13 @@
 import type { Knex } from 'knex';
 
-const config: Record<string, Knex.Config> = {
-  development: {
-    client: 'pg',
-    connection: {
-      host: process.env.DB_HOST ?? 'localhost',
-      port: Number(process.env.DB_PORT ?? 5432),
-      user: process.env.DB_USER ?? 'postgres',
-      database: process.env.DB_NAME ?? 'ortho',
-      password: process.env.DB_PASSWORD ?? 'postgres',
-    },
-    searchPath: ['platform_messaging', 'public'],
-    migrations: {
-      directory: './migrations',
-      loadExtensions: ['.ts'],
-      schemaName: 'platform_messaging',
-      tableName: 'knex_migrations',
-    },
-  },
-  production: {
-    client: 'pg',
-    connection: {
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT ?? 5432),
-      user: process.env.DB_USER,
-      database: process.env.DB_NAME,
-      password: process.env.DB_PASSWORD,
-    },
-    searchPath: ['platform_messaging', 'public'],
-    migrations: {
-      directory: './migrations',
-      loadExtensions: ['.ts'],
-      schemaName: 'platform_messaging',
-      tableName: 'knex_migrations',
-    },
+const config: Knex.Config = {
+  client: 'pg',
+  connection: process.env['DATABASE_URL'],
+  searchPath: ['platform_messaging', 'public'],
+  migrations: {
+    directory: './migrations',
+    schemaName: 'platform_messaging',
+    tableName: 'knex_migrations',
   },
 };
 
