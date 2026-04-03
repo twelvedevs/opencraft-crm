858df715b734aa4fe72e8d663256c86a5b510fe6
Andrey Pankov <a.pankov@gmail.com>
2026-04-03 15:30:05 +0300

    chore: add missing knexfile schema config for docker-compose migrations

--- apps/platform/messaging/knexfile.ts
+++ apps/platform/messaging/knexfile.ts
@@ -3,8 +3,11 @@ import type { Knex } from 'knex';
 const config: Knex.Config = {
   client: 'pg',
   connection: process.env['DATABASE_URL'],
+  searchPath: ['platform_messaging', 'public'],
   migrations: {
     directory: './migrations',
+    schemaName: 'platform_messaging',
+    tableName: 'knex_migrations',
   },
   seeds: {
     directory: './seeds',
